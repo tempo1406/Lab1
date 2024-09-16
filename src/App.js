5865040f4924646e68c87feafc11decddf8446e5
tempo1406 <134191285+tempo1406@users.noreply.github.com>
2024-09-16 08:39:27 +0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './HelloWord';
+
+function App() {
+  return (
+    <div>
+      <HelloWorld/>
+    </div>
+    
+  );
+}
+
+export default App;
---
+++ src/HelloWord.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './HelloWord.css'
+function HelloWorld() {
+    return (
+    <div>
+        <h1>Hello, World!</h1>
+    </div>
+    );
+}
+
+export default HelloWorld;
